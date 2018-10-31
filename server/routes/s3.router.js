7933610b82f883a4564f4a37b16b72fda970b514
Paul Tiller <pftiller@gmail.com>
2018-10-31 11:12:32 -0500

    adding functionality to remove existing file

--- server/routes/s3.router.js
+++ server/routes/s3.router.js
@@ -7,17 +7,21 @@ const bucketName = 'thinknear-share';
 var s3 = new AWS.S3()
 let today = new Date();
 let year = today.getFullYear();
-let month = today.getMonth();
-let yesterday = (today.getDate()) - 1;
+let month = today.getMonth() +1;
+let yesterday = (today.getDate()) - 2;
+
 router.use('/api', (req, res, next) => {
     let key;
+    if(fs.existsSync('output.txt')){
+        fs.unlinkSync('output.txt');
+    }
     for (let i = 0; i < 39; i++) {
         if (i < 10) {
-            key = `partner=DFMStPaul/version=2013-09/totals/year=${year}/month=${month}/day=${yesterday}000${i}_part_00`;
+            key = `partner=DFMStPaul/version=2013-09/totals/year=${year}/month=${month}//000${i}_part_00`;
             downloadFile(bucketName, key)
         }
         else {
-            key = `partner=DFMStPaul/version=2013-09/totals/year=2018/month=10/00${i}_part_00`;
+            key = `partner=DFMStPaul/version=2013-09/totals/year=${year}/month=${month}/00${i}_part_00`;
             downloadFile(bucketName, key)
         }
     }
@@ -36,14 +40,27 @@ let downloadFile = (bucketName, key) => {
         }
         else {
             fs.appendFile('output.txt', data.Body, function (err) {
-                if (err) throw err;
+                if (err) { 
+                    throw err;
+                }
                 console.log('Saved!');
-              });
-          
-        }
-        
-    })
+                });
+            }
+        })
 }
+// var params = {
+//     Bucket: 'thinknear-share',
+//     Prefix: 'partner=DFMStPaul/'
+// }
+//     s3.listObjects(params, (err, data)=>{
+//         if (err) {
+//             console.log(err)
+//         }
+//         else {
+//             console.log(data);
+//         }
+//     })
+// })
 
 
 
